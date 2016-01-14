1db78efc69de345bf4f4414f6375e12e9c2ee0b5
Tang Zijian <tangzj77@gmail.com>
2016-01-14 22:21:10 +0800

    add pods

---
+++ ARShop-Retailer/Pods/Target Support Files/Pods-ARShop-Retailer/Pods-ARShop-Retailer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARShop_Retailer : NSObject
+@end
+@implementation PodsDummy_Pods_ARShop_Retailer
+@end
---
+++ ARShop-Retailer/Pods/Target Support Files/Pods-ARShop-Retailer/Pods-ARShop-Retailer-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ARShop_RetailerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ARShop_RetailerVersionString[];
+
